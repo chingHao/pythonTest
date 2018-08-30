80a9f48bb36d2cf3d80a123da0a6ef751dfe2719
chinghao <573634450@qq.com>
2018-08-30 16:45:05 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,57 @@
+# print('hello  ,python ,java ,android, ios')
+# classmate = ['java','ios','python']
+# print(len(classmate))
+# print(classmate[0])
+# message = "hello python world"
+# print(message)
+# message = "hello python crash course world"
+# print(message)
+# name = "Ada loveLace"
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = first_name+" "+last_name
+# print(full_name)
+# name = "Eric"
+# print("Hello " + name + "," " would you like to learn some Python today?")
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+# print(3/2)
+# print(1+7)
+# print(9-1)
+# print(2*4)
+# print(24/3)
+# bicycles = ["trek",'cannondale','redline','specialized']
+# print(bicycles)
+# print(bicycles[0].title())
+# name = bicycles[0]
+# print(name.title())
+# message = "My first bicycle was a " + bicycles[0].title()+"."
+# print(message)
+# motorcycles = ['honda','yamaha','suzuki']
+# print(motorcycles)
+# motorcycles[0] = 'ducati'
+# print(motorcycles)
+# motorcycles.append('ducati')
+# print(motorcycles)
+# del motorcycles[0]
+# print(motorcycles)
+# popped_motorcycles = motorcycles.pop()
+# print(motorcycles)
+# print(popped_motorcycles)
+# motorcycles.remove("honda")
+# motorcycles.sort()
+# print(motorcycles)
+# motorcycles.sort(reverse=True)
+# print(motorcycles)
+# trips = ['beijing','tianjin','yunnan','guizhou','dali']
+# print(trips)
+# # print(sorted(trips))
+# trips.reverse()
+# print(trips)
+magicans = ['alice','david','carolina']
+for magican in magicans:
+    print(magican)
